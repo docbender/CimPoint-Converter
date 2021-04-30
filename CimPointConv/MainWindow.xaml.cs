98e72443359b7c214d60c616d306432297f7a2e2
docbender <vita.tucek@seznam.cz>
2021-04-30 20:48:32 +0200

    Images

--- CimPointConv/MainWindow.xaml.cs
+++ CimPointConv/MainWindow.xaml.cs
@@ -86,7 +86,7 @@ namespace CimPointConv
         {
             get
             {
-                return $"{Assembly.GetEntryAssembly().GetName().Version.ToString(3)} alpha";
+                return $"{Assembly.GetEntryAssembly().GetName().Version.ToString(3)} beta 1";
             }
         }
 
